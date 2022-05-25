99aa0015298b6f3e5508bf6802274534c98fb662
SohelTanbir <soheltanbir19@gmail.com>
2022-05-25 18:44:29 +0600

    add a function to toggle sub list items

--- js/main.js
+++ js/main.js
@@ -3,40 +3,30 @@ $(document).ready(function(){
 // toggle sidebar
 $(".toggle-icon").click(function(){
     $(".main-content").toggleClass("main-content-toggle");
-    $(".toggle-icon .fa-bars").toggle()
-    $(".toggle-icon .fa-xmark").toggle()
+    $(".toggle-icon .fa-bars").toggle();
+    $(".toggle-icon .fa-xmark").toggle();
+    $(".sidebar .sidebar-list li a .left-icon").toggleClass("mr-22");
 })
 
 
 // toggle sidebar sub list item
-$(".sidebar-list li.user").click(function(){
-    $(".sidebar-list li.user .angle_down").toggle()
-    $(".sidebar-list li.user .angle_up").toggle();
-    $(".sidebar-list li.user .sidebar-sub-list").slideToggle();
-})
-$(".sidebar-list li.message").click(function(){
-    $(".sidebar-list li.message .angle_down").toggle()
-    $(".sidebar-list li.message .angle_up").toggle();
-    $(".sidebar-list li.message .sidebar-sub-list").slideToggle();
-})
-$(".sidebar-list li.bookmark").click(function(){
-    $(".sidebar-list li.bookmark .angle_down").toggle()
-    $(".sidebar-list li.bookmark .angle_up").toggle();
-    $(".sidebar-list li.bookmark .sidebar-sub-list").slideToggle();
-})
-$(".sidebar-list li.file").click(function(){
-    $(".sidebar-list li.file .angle_down").toggle()
-    $(".sidebar-list li.file .angle_up").toggle();
-    $(".sidebar-list li.file .sidebar-sub-list").slideToggle();
-})
-$(".sidebar-list li.stats").click(function(){
-    $(".sidebar-list li.stats .angle_down").toggle()
-    $(".sidebar-list li.stats .angle_up").toggle();
-    $(".sidebar-list li.stats .sidebar-sub-list").slideToggle();
-})
 $(".sidebar-list li").click(function(){
-    console.log($(".sidebar-list li"));
+    const targetElement = this.children[1];
+    const showItem = document.querySelector(".show")
+    // remove open sub item
+    if(!targetElement.classList.contains("show") && showItem){
+        showItem.classList.remove("show");
+    }
+    // toggle show class 
+    targetElement.classList.toggle("show");
+    console.log(targetElement.parentNode.children[0].children[0])
+    console.log(targetElement.parentNode.children[0].children[1])
+    // targetElement.parentNode.children[0].children[0].toggle();
+    // targetElement.parentNode.children[0].children[1].toggle();
+
 });
+
+
 
 
 
